444e587165175f660e776ba93ba0403ccd3d418e
erzaosmani <erzaosmanii578@gmail.com>
2022-12-30 21:11:01 +0100

    added the validation of the sign up form

---
+++ src/components/SignUp/SignUpForm/useSignUpFormik.ts
@@ -0,0 +1,46 @@
+import { useFormik, FormikHelpers } from "formik";
+import * as Yup from "yup";
+import { string } from "yup/lib/locale";
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string().required("Username is required"),
+  email: Yup.string().required("Email is required"),
+  password: Yup.string().required("Password is required!").min(8),
+  confirm: Yup.string().required("It is required to confirm your password!"),
+  age: Yup.string().required("Age is required!"),
+  gender: Yup.string().required("Gender is required!"),
+ 
+});
+
+interface LoginFields {
+  username: string;
+  email: string;
+  password: string;
+  confirm: string;
+  age: string;
+  gender: string;
+}
+
+interface UseSignUpFormOptions {
+  onSubmit: (
+    values: LoginFields,
+    formikHelpers: FormikHelpers<LoginFields>
+  ) => void;
+}
+
+export const useSignUpFormik = ({ onSubmit }: UseSignUpFormOptions) => {
+  return useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirm: "",
+      age: "",
+      gender: "",
+    },
+    validateOnBlur: false,
+    validateOnChange: true,
+    validationSchema: LoginSchema,
+    onSubmit,
+  });
+};
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,108 +1,146 @@
 import {
-    Button,
-    Checkbox,
-    FormControlLabel,
-    InputLabel,
-    MenuItem,
-    Select,
-    TextField,
-  } from "@mui/material";
-  import { Box } from "@mui/system";
-  import React from "react";
-  
-  export const SignUp = () => {
-    return (
+  Button,
+  Checkbox,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { useSignUpFormik } from "../components/SignUp/SignUpForm/useSignUpFormik";
+
+export const SignUp = () => {
+  const formik = useSignUpFormik({
+    onSubmit(values, formikHelpers) {
+      console.log("Formik values", values);
+    },
+  });
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Box
         sx={{
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          border: 2,
+          borderColor: "secondary.main",
+          borderRadius: 1,
+          padding: "10px",
+          minWidth: 360,
         }}
       >
+        <h1>Create an account</h1>
+
         <Box
+          component="form"
           sx={{
-            border: 2,
-            borderColor: "secondary.main",
-            borderRadius: 1,
-            padding: "10px",
-            minWidth: 360,
+            display: "flex",
+            flexDirection: "column",
+            marginTop: 4,
           }}
+          noValidate
+          autoComplete="off"
+          onSubmit={formik.handleSubmit}
         >
-          <h1>Create an account</h1>
-  
-          <Box
-            component="form"
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              marginTop: 4,
-            }}
-            noValidate
-            autoComplete="off"
+          <TextField
+            id="username"
+            label="Username"
+            variant="outlined"
+            name="username"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            sx={{ marginBottom: 2 }}
+            error={formik.touched.username && !!formik.errors.username}
+            helperText={formik.errors.username}
+          />
+
+          <TextField
+            id="email"
+            label="Email"
+            variant="outlined"
+            name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            sx={{ marginBottom: 2 }}
+            error={formik.touched.email && !!formik.errors.email}
+            helperText={formik.errors.email}
+          />
+
+          <TextField
+            id="password"
+            label="Password"
+            variant="outlined"
+            name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            sx={{ marginBottom: 2 }}
+            error={formik.touched.password && !!formik.errors.password}
+            helperText={formik.errors.password}
+          />
+
+          <TextField
+            id="confirm"
+            label="Confirm password"
+            variant="outlined"
+            name="confirm"
+            value={formik.values.confirm}
+            onChange={formik.handleChange}
+            sx={{ marginBottom: 2 }}
+            error={formik.touched.confirm && !!formik.errors.confirm}
+            helperText={formik.errors.confirm}
+          />
+
+          <TextField
+            id="age"
+            label="Age"
+            variant="outlined"
+            name="age"
+            value={formik.values.age}
+            onChange={formik.handleChange}
+            sx={{ marginBottom: 2 }}
+            error={formik.touched.age && !!formik.errors.age}
+            helperText={formik.errors.age}
+          />
+
+          <InputLabel id="select-gender">Gender</InputLabel>
+          <Select
+            label="Gender"
+            name="gender"
+            value={formik.values.gender}
+            onChange={formik.handleChange}
+            sx={{ marginBottom: 2 }}
+            error={formik.touched.gender && !!formik.errors.gender}
           >
-            <TextField
-              id="username"
-              label="Username"
-              variant="outlined"
-              sx={{ marginBottom: 2 }}
-            />
-  
-            <TextField
-              id="email"
-              label="Email"
-              variant="outlined"
-              sx={{ marginBottom: 2 }}
-            />
-  
-            <TextField
-              id="password"
-              label="Password"
-              variant="outlined"
-              sx={{ marginBottom: 2 }}
-            />
-  
-            <TextField
-              id="confirm-password"
-              label="Confirm password"
-              variant="outlined"
-              sx={{ marginBottom: 2 }}
-            />
-  
-            <TextField
-              id="age"
-              label="Age"
-              variant="outlined"
-              sx={{ marginBottom: 2 }}
-            />
-  
-            <InputLabel id="select-gender">Gender</InputLabel>
-            <Select label="Gender">
-              <MenuItem value={1}>F</MenuItem>
-              <MenuItem value={2}>M</MenuItem>
-            </Select>
-  
-            <InputLabel id="select-preferences">Preferences</InputLabel>
-            <Select label="Preferences">
-              <MenuItem value={1}>Action</MenuItem>
-              <MenuItem value={2}>Comedy</MenuItem>
-              <MenuItem value={3}>Sci-Fi</MenuItem>
-              <MenuItem value={4}>Drama</MenuItem>
-              <MenuItem value={5}>Thriller</MenuItem>
-              <MenuItem value={6}>Other</MenuItem>
-            </Select>
-  
-            <FormControlLabel
-              control={<Checkbox />}
-              label="Terms and Conditions"
-            />
-  
-            <Button type="submit" variant="contained">
-              Sign Up
-            </Button>
-          </Box>
+            <MenuItem value={1}>F</MenuItem>
+            <MenuItem value={2}>M</MenuItem>
+          </Select>
+
+          <InputLabel id="select-preferences">Preferences</InputLabel>
+          <Select label="Preferences" sx={{ marginBottom: 2 }}>
+            <MenuItem value={1}>Action</MenuItem>
+            <MenuItem value={2}>Comedy</MenuItem>
+            <MenuItem value={3}>Sci-Fi</MenuItem>
+            <MenuItem value={4}>Drama</MenuItem>
+            <MenuItem value={5}>Thriller</MenuItem>
+            <MenuItem value={6}>Other</MenuItem>
+          </Select>
+
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Terms and Conditions"
+          />
+
+          <Button type="submit" variant="contained">
+            Sign Up
+          </Button>
         </Box>
       </Box>
-    );
-  };
-  
+    </Box>
+  );
+};
