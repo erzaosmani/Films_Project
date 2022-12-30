c5db1f8e7fdd00a40785777385c1030409907580
erzaosmani <erzaosmanii578@gmail.com>
2022-12-30 19:53:59 +0100

    added the forgot password form

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { Box, Button, TextField } from "@mui/material";
+import React from "react";
 
 export const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
-  )
-}
+    <Box
+      sx={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          border: 2,
+          borderColor: "secondary.main",
+          borderRadius: 1,
+          padding: "10px",
+          minWidth: 360,
+        }}
+      >
+        <h3>Forgot your password?</h3>
+
+        <Box
+          component="form"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            marginTop: 4,
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="email"
+            label="Email"
+            variant="outlined"
+            sx={{ marginBottom: 2 }}
+          />
+
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
