4b428bc60b7864cbf2364b9868ac376996e47b5c
erzaosmani <erzaosmanii578@gmail.com>
2022-12-31 01:52:46 +0100

    fixed the age validation

--- src/components/SignUp/SignUpForm/useSignUpFormik.ts
+++ src/components/SignUp/SignUpForm/useSignUpFormik.ts
@@ -5,9 +5,13 @@ import { string } from "yup/lib/locale";
 const LoginSchema = Yup.object().shape({
   username: Yup.string().required("Username is required!"),
   email: Yup.string().required("Email is required!"),
-  password: Yup.string().required("Password is required!").min(8),
+  password: Yup.string()
+    .required("Password is required!")
+    .min(8, "Password must be at least 8 characters!"),
   confirm: Yup.string().required("Confirming your password is required!"),
-  age: Yup.string().required("Age is required!"),
+  age: Yup.number()
+    .required("Age is required!")
+    .min(16, "You must be at least 16 years!"),
   gender: Yup.string().required("Gender is required!"),
 });
 
